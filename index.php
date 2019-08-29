be87d7de26975a580257d54351d6b881bcc4bfa5
fumiya <fu38.terada@gmail.com>
2019-08-29 15:32:05 +0900

    header/footerファイルの切り分け

--- index.php
+++ index.php
@@ -1,16 +1,3 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
-  <?php wp_head();?>
-</head>
-<body>
-  <header>ここヘッダー</header>
+<?php get_header();?>
   <h1><?php the_title();?></h1>
-  <?php wp_footer();?>
-  <footer>ここヘッダー</footer>
-</body>
-</html>
+<?php get_footer();?>
