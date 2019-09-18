5dc786c34359e12194314a3e312537326338229a
fumiya <fu38.terada@gmail.com>
2019-09-18 15:12:54 +0900

    完成版

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
 <?php get_header();?>
   <h1><?php the_title();?></h1>
+  <img src="<?php echo get_template_directory_uri(); ?>/images/top_image.jpg" alt="" width="300">
+  <?php echo test();?>
 <?php get_footer();?>
